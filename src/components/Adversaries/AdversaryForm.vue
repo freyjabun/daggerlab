ebb92c55fd6e42849cc74eff4fa43adde1e15aef
freyjabun <freyjabun@outlook.dk>
2025-11-01 16:52:22 +0100

    AdversaryForm design changes

--- src/components/Adversaries/AdversaryForm.vue
+++ src/components/Adversaries/AdversaryForm.vue
@@ -15,11 +15,10 @@ defineProps({
 </script>
 
 <template>
-  <div class="bg-daggerpurple-dark p-6 rounded-2xl shadow-md space-y-5 border border-gray-300">
-    <h2 class="text-2xl font-bold text-white mb-2">Adversary Editor</h2>
+  <div class="bg-daggerpurple-dark p-4 rounded-2xl shadow-md space-y-4 border border-gray-300">
+    <h2 class="text-2xl font-bold text-white">Adversary Editor</h2>
 
     <BaseInfo v-model="adversary.baseInfo"></BaseInfo>
-
     <!-- Stats -->
     <Collapsible title="Stats">
       <Stats v-model="adversary.stats" />
@@ -37,14 +36,14 @@ defineProps({
       <FeaturesEditor v-model="adversary.features"></FeaturesEditor>
     </Collapsible>
 
-    <div class="flex mt-2">
-      <button v-if="showAdd" @click="$emit('add')"
-        class="leading-none bg-sage text-white px-4 py-2 rounded mr-auto">
-        Add Adversary
-      </button>
-      <button v-if="showRemove" @click="$emit('remove')" class="leading-none bg-blade text-white px-4 py-2 rounded ml-auto">
-        Remove
-      </button>
-    </div>
+  </div>
+  <div class="flex mt-2 ml-2 mr-2">
+    <button v-if="showAdd" @click="$emit('add')"
+      class="leading-none bg-sage text-white px-4 py-2 rounded mr-auto">
+      Add Adversary
+    </button>
+    <button v-if="showRemove" @click="$emit('remove')" class="leading-none bg-blade text-white px-4 py-2 rounded ml-auto">
+      Remove
+    </button>
   </div>
 </template>
