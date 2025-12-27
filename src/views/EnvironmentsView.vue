746c2f337d0947754feea6060842e2b03009d942
freyjabun <freyjabun@outlook.dk>
2025-12-27 13:24:06 +0100

    beautiful environmentview that breaks the site

--- src/views/EnvironmentsView.vue
+++ src/views/EnvironmentsView.vue
@@ -9,10 +9,35 @@ const store = useEnvironmentStore()
 
 const environments = reactive([createExampleEnvironment(),])
 
+const clearAllEnvironments = () => {
+    if (confirm('Are you sure you want to remove all environments?')) {
+    store.removeAllEnvironments()
+}}
+
 </script>
 <template>
-  <div class="environments-view">
-    <h1>Nothing to see here</h1>
+  <div class="flex justify-between mb-4">
+    <button @click="printAll" class="button mb-2 bg-gradient-to-r from-splendor-dark to-splendor-light">Print Environments Except It Doesn't Work</button>
+    <button @click="clearAllEnvironments"
+       class="button mb-2 bg-blade">Clear All</button>
+  </div>
+
+  <div class="flex flex-col gap-4">
+    <div v-for="(env, i) in store.environments" :key="i" class="flex flex-col lg:flex-row gap-8 items-start">
+      <div class="w-l flex-shrink-0 print:hidden">
+        <EnvironmentForm :environment="env"
+        @remove="store.removeEnvironment(i)"
+        :show-remove="i !== 0"
+        @add="store.addEnvironment()"
+        :show-add="i === store.adversaries.length - 1"
+        />
+      </div>
+      
+      <div class="w-xl bg-sitebg p-6 print-card">
+        <EnvironmentCard :environment="env"/>
+      </div>
+
+    </div>
   </div>
 </template>
 
